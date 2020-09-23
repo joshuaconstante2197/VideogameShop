ea959355f733003f205e8962f530e91900b6b846
[Joshua Constante] <[joshuaconstante2197@hotmail.com]>
2020-09-23 08:59:32 -0400

    Fix display currency

--- VideogameShop.Web/wwwroot/js/site.js
+++ VideogameShop.Web/wwwroot/js/site.js
@@ -1,6 +1,10 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+//currency formatter
+const numberFormatter = new Intl.NumberFormat('en-Us', {
+    style: 'currency',
+    currency: 'USD',
+});
 // Write your JavaScript code.
 $(document).ready(function () {
     console.log("ready")
